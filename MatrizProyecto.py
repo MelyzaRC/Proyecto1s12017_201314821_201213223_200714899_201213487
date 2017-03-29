1c2a5cb53f2e0e312c1dcc22dff38913fe7404e7
MelyzaRC <mlzdrg792@gmail.com>
2017-03-28 23:46:48 -0600

    Python y Reportes Final
    Python y Reportes, ip correcta

--- MatrizProyecto.py
+++ MatrizProyecto.py
@@ -1407,4 +1407,4 @@ def verUsuarios():
 	return s 
 	
 if __name__ == "__main__":
-  app.run(debug=True, host='localhost')
+  app.run(debug=True, host='192.168.43.223')
--- Reportes/src/reportes/PantallaReportes.java
+++ Reportes/src/reportes/PantallaReportes.java
@@ -37,7 +37,7 @@ public class PantallaReportes extends javax.swing.JFrame {
 
     public String metodoWS(String metodo, RequestBody formBody) {
         try {
-            URL url = new URL("http://localhost:5000/" + metodo);
+            URL url = new URL("http://192.168.43.223:5000/" + metodo);
             Request request = new Request.Builder().url(url).post(formBody).build();
             Response response = webClient.newCall(request).execute();//Aqui obtiene la respuesta en dado caso si hayas pues un return en python
             String response_string = response.body().string();//y este seria el string de las respuesta
